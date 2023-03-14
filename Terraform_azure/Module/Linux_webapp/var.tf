1783d04e38913f58f078eb8e61ba3833dc8fc5fc
uday <udaya.marolix@gmail.com>
2023-03-14 16:52:44 +0530

    updated

---
+++ Terraform_azure/Module/Linux_webapp/var.tf
@@ -0,0 +1,16 @@
+variable "rg_name"{
+type = string
+default="foodalerrt-rg"
+}
+variable "rg_location" {
+    type = String
+    default = "eastus"
+}
+variable "as_name" {
+    type = String
+    default = "foodalertappservice"
+}
+variable "wa_name" {
+    type = String
+    default = "foodalertappservice"
+}
---
+++ Terraform_azure/Module/Linux_webapp/webapp.tf
@@ -0,0 +1,40 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
+}
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  rg_name     = var.rg_name
+  location = var.location
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  as_name                = var.as_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  wa_name               = var.wa_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+  }
+}
