1d166fb2210feb77c622eaf9b7fd4e2bbf475929
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 15:31:59 +0530

    Create main.tf

---
+++ foodalert/main.tf
@@ -0,0 +1,27 @@
+module "webapp" {
+  source = "../Modules/"
+
+  rg_name          = "foodalertresourecegroup232"
+  rg_location        = "East Us"
+  asp_name     =  "appserviceplanfoodalertdev"
+  as_name             = "appservicefoodalertdev"
+  #env                 = var.r_env
+  #asp_name            = "${var.r_prefix}-asp1-${var.r_env}-${random_integer.ri.result}"
+  #tier                = "Standard"
+  #size                = "S1"
+  #webapplist          = ["identity", "webapp"]
+  #resource_group_name = azurerm_resource_group.rg.name
+  #location            = azurerm_resource_group.rg.location
+  #scm_ip_restriction  = ["31.121.101.144/28", "31.121.101.128/29", "202.89.106.0/23"]
+  #app_settings = {
+   # Angular2URL    = local.v2_url
+    #AppURL         = local.webapp_url
+    #AtlasAPI       = local.api_url
+    #AtlasSTSOrigin = local.identity_url
+    #EnableCorsFor  = local.cors
+    #WizardApp      = local.wizard_url
+  }
+
+}
+
+
