dccdd38f0322d01fa86ca44705c116aebb6c4921
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 12:04:14 +0530

    Update main.tf

--- ashok/main.tf
+++ ashok/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "foodalertresourcegroup12"
   location = "East US"
 }
-resource "app_service_plan" "example" {
+resource "azurerm_app_service_plan" "example" {
   name                = "ashokappserviceplan12"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
