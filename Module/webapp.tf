4bfc32f86312510f5d2468d5647c46087b1ae528
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 16:47:37 +0530

    Update webapp.tf

--- Module/webapp.tf
+++ Module/webapp.tf
@@ -12,8 +12,8 @@ resource "azurerm_app_service_plan" "aspname" {
 }
 resource "azurerm_app_service" "asname" {
   name                = var.as_name
-  location            = azurerm_resource_group.rgname.location
-  resource_group_name = azurerm_resource_group.rgname.name
+  location            = var.rg_location
+  resource_group_name = var.rg_name
   app_service_plan_id = azurerm_app_service_plan.aspname.id
 
   site_config {
