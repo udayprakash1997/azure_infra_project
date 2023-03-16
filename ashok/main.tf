a1ed0d3ea9dd8d9f3dedd860e7485450c378b30f
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 09:09:58 +0530

    Update main.tf

--- ashok/main.tf
+++ ashok/main.tf
@@ -1,6 +1,6 @@
  module "rg" {
   # Terraform Cloud PMR use
-  source = "../../Terraform_azure/Module/Linux_webapp"
+  source = "../Terraform_azure/Module/Linux_webapp"
   version = "~>1.3.3"
 
   rg_name     = "foodalert-resourcegroup"
