9f92b3568fe36a2d48b61955f14d27ffedcb3d55
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 08:17:15 +0530

    Update webapp.tf

--- Terraform_azure/Module/Linux_webapp/webapp.tf
+++ Terraform_azure/Module/Linux_webapp/webapp.tf
@@ -17,24 +17,3 @@ resource "azurerm_resource_group" "rg" {
   rg_name     = var.rg_name
   location = var.location
 }
-
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  as_name                = var.as_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
-}
-
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  wa_name               = var.wa_name
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  https_only            = true
-  site_config { 
-    minimum_tls_version = "1.2"
-  }
-}
