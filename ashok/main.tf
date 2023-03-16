57364a7e47987f504aa227115a4083ea99dfbfc5
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 10:17:24 +0530

    Update main.tf

--- ashok/main.tf
+++ ashok/main.tf
@@ -1,17 +1,33 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = "foodalertresourcegroup123"
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "api-appserviceplan-proashok"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
-  sku {
-    tier = "Standard"
-    size = "S1"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
   }
 }
