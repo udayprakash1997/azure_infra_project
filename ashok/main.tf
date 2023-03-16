5c32e3603e9f57fbd808d1891976ee54498bcb1d
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 08:43:18 +0530

    Create main.tf

---
+++ ashok/main.tf
@@ -0,0 +1,8 @@
+ module "rg" {
+  # Terraform Cloud PMR use
+  source = "../../Terraform_azure/Module/Linux_webapp"
+  version = "~>1.3.3"
+
+  rg_name     = "foodalert-resourcegroup"
+  rg_location      = "East US"
+}
