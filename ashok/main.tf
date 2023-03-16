22beec3ceb4e6d44e1f354e01dfebcf8e65199f6
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 10:40:58 +0530

    Update main.tf

--- ashok/main.tf
+++ ashok/main.tf
@@ -2,5 +2,10 @@ resource "azurerm_resource_group" "example" {
   name     = "foodalertresourcegroup12"
   location = "East US"
 }
-
-
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-foodalert-linux"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
