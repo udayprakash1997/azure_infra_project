8a9bd7818e66bc60ef1726b6c12e2283f905a28d
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 12:07:39 +0530

    Update main.tf

--- ashok/main.tf
+++ ashok/main.tf
@@ -2,18 +2,18 @@ resource "azurerm_resource_group" "example" {
   name     = "foodalertresourcegroup12"
   location = "East US"
 }
-resource "azurerm_app_service_plan" "example" {
-  name                = "ashokappserviceplan12"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "Linux"
-  reserved            = true
+#resource "azurerm_app_service_plan" "example" #{
+ # name                = "ashokappserviceplan12"
+ # location            = azurerm_resource_group.example.location
+ # resource_group_name = azurerm_resource_group.example.name
+ # kind                = "Linux"
+ # reserved            = true
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
+ # sku #{
+    #tier = "Standard"
+    #size = "S1"
+  #}
+#}
 #resource "azurerm_app_service" "example" #{
   #name                = "exampleapp1"
   #resource_group_name = azurerm_resource_group.example.name
