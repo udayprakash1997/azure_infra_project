b2815c0f46a1687e213ef5614c825c677dcf00cc
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 09:36:00 +0530

    Update main.tf

--- ashok/main.tf
+++ ashok/main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "rg" {
   name     = "foodalertresourcegroup123"
   location = "East US"
 }
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "api-appserviceplan-pro"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
