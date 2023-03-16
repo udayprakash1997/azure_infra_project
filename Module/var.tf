0f33fb223a7ef78267320b92ea8f0b0e45bd0b74
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-16 15:19:01 +0530

    Create var.tf

---
+++ Module/var.tf
@@ -0,0 +1,17 @@
+variable "rg_name"{
+type = string
+default=" "
+}
+
+variable "rg_location" {
+    type = String
+    default = " "
+}
+variable "asp_name" {
+  type = string
+  default=" "
+}
+variable "as_name" {
+  type = string
+  default=" "
+}
