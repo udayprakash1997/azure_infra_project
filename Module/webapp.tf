24dd869baed7afdf65c264a0250262bb3fdc6ed6
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-17 10:24:49 +0530

    Update webapp.tf

--- Module/webapp.tf
+++ Module/webapp.tf
@@ -2,7 +2,7 @@ resource "azurerm_app_service" "asname" {
   name                = var.as_name
   location            = var.rg_location
   resource_group_name = var.rg_name
-  app_service_plan_id = var.aspname
+  app_service_plan_id = var.asp_name
 
   site_config {
     dotnet_framework_version = "v4.0"
