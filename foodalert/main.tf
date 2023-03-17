8e7160d26fec1d3dd726fb07298b24047eb6e444
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-17 15:14:13 +0530

    Update main.tf

--- foodalert/main.tf
+++ foodalert/main.tf
@@ -22,18 +22,18 @@ module "webapp" {
   asp_name            = "${var.r_prefix}-asp1-${var.r_env}-${random_integer.ri.result}"
   tier                = "Standard"
   size                = "S1"
-  webapplist          = ["identity", "webapp"]
+  #webapplist          = ["identity", "webapp"]
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  scm_ip_restriction  = ["31.121.101.144/28", "31.121.101.128/29", "202.89.106.0/23"]
-  app_settings = {
-    Angular2URL    = local.v2_url
-    AppURL         = local.webapp_url
-    AtlasAPI       = local.api_url
-    AtlasSTSOrigin = local.identity_url
-    EnableCorsFor  = local.cors
-    WizardApp      = local.wizard_url
-  }
+  #scm_ip_restriction  = ["31.121.101.144/28", "31.121.101.128/29", "202.89.106.0/23"]
+  #app_settings= #{
+   # Angular2URL    = local.v2_url
+    #AppURL         = local.webapp_url
+    #AtlasAPI       = local.api_url
+    #AtlasSTSOrigin = local.identity_url
+    #EnableCorsFor  = local.cors
+    #WizardApp      = local.wizard_url
+  #}
 
 }
 
