60a25fba5ae7ab10e038f75d580c383928401d32
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-29 10:41:50 +0530

    Update webapp.tf

--- Module/webapp.tf
+++ Module/webapp.tf
@@ -1,24 +1,9 @@
-resource "azurerm_resource_group" "rgname" {
-  name     = var.rg_name
-  location = var.rg_location
-}
-resource "azurerm_app_service_plan" "aspname" {
-  name                = var.asp_name
-  location            = azurerm_resource_group.rgname.location
-  resource_group_name = azurerm_resource_group.rgname.name
-  kind                = "Linux"
-  reserved            = true
 
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-}
 resource "azurerm_app_service" "asname" {
   name                = var.as_name
-  location            = azurerm_resource_group.rgname.location
-  resource_group_name = azurerm_resource_group.rgname.name
-  app_service_plan_id = azurerm_app_service_plan.aspname.id
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  app_service_plan_id = var.asp_name
 
   site_config {
     dotnet_framework_version = "v4.0"
