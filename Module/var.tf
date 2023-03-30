ca707f3109442a7c0ccc8c4c89b6ae96502251d3
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-30 17:49:54 +0530

    Update var.tf

--- Module/var.tf
+++ Module/var.tf
@@ -18,3 +18,72 @@ variable "as_name" {
   type = string
   default=" "
 }
+variable "Application_Type" {
+  type = string
+  default="App service"
+}
+variable "Application_Name" {
+  type = string
+  default="Food alert"
+}
+variable "Application_Environment" {
+  type = string
+  default="Dev"
+}
+variable "Brand_name" {
+  type = string
+  default="Food alert"
+}
+variable "Business_Unit" {
+  type = string
+  default="Food alert"
+}
+variable "Product" {
+  type = string
+  default="Food alert"
+}
+variable "Cost_center" {
+  type = string
+  default="Food alert Dev"
+}
+variable "Deploy_type" {
+  type = string
+  default="Terraform"
+}
+variable "Access_type" {
+  type = string
+  default="Private"
+}
+variable "Shared_Resources" {
+  type = string
+  default="Yes"
+}
+variable "Shared_by" {
+  type = string
+  default="Dev"
+}
+variable "Service_Type" {
+  type = string
+  default="API"
+}
+variable "Created_On" {
+  type = string
+  default="30/03/2023 "
+}
+variable "JIRA_ID" {
+  type = string
+  default=" "
+}
+variable "JIRA_ID" {
+  type = string
+  default="No"
+}
+variable "Auto_scale" {
+  type = string
+  default="No"
+}
+
+variable "Start_Stop" {
+  type = string
+  default="10 AM"
+}
