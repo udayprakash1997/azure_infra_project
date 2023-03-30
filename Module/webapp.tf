04cd64c21ef8f38af39f7a2abf9eadf73b1f6533
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-03-30 17:55:43 +0530

    Update webapp.tf

--- Module/webapp.tf
+++ Module/webapp.tf
@@ -17,4 +17,23 @@ resource "azurerm_app_service" "asname" {
     "DOCKER_REGISTRY_SERVER_URL"                = "value"
     "DOCKER_REGISTRY_SERVER_USERNAME"           = "value"
   }
+  tags ={
+    Application_Type=var.Application_Type
+    Application_Name=var.Application_Name
+    Application_Environment=var.Application_Environment
+    Brand_name=var.Brand_name
+    Business_Unit=var.Business_Unit
+    Product=var.Product
+    Cost_center=var.Cost_center
+    Deploy_type=var.Deploy_type
+    Access_type=var.Access_type
+    Shared_Resources=var.Shared_Resources
+    Shared_by=var.Shared_by
+    Service_Type=var.Service_Type
+    Created_On=var.Created_On
+    JIRA_ID=var.JIRA_ID
+    Auto_scale=var.Auto_scale
+    Start_Stop=var.Start_Stop
+    
+  }
 }
