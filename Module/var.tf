20b7948ccd9b4d5c0376ccfb0f3c9f5fbaec410f
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-04-05 10:39:08 +0530

    Update var.tf

--- Module/var.tf
+++ Module/var.tf
@@ -1,77 +1,85 @@
-# Variables for Webapp Module
-
-variable "webapplist" {  
-  description = "Provide the names for apps"
-  type = list
-}
-
-variable "image_name" {
-  description = "Provide name for the docker image"
-  
-}
-
-variable "existing_asp_rg" {
-  description = "Name of existing resource group"
-  
-}
-
-variable "existing_asp" {
-  description = "Name of existing app service plan"
-  
-}
-variable "app_settings" {
-  type        = map(string)
-  default     = {}
-  description = "Map of App Settings."
-}
-
-variable "scm_ip_restriction" {
-  type        = list(string)
-  default     = []
-  description = "A list of IP addresses in CIDR format specifying Access Restrictions."
-}
-
-
-variable "create_asp" {
-  type = bool
-  
-}
-
-variable "prefix" {
-  type        = string
-  description = "The Application prefix name"
-}
-
-variable "asp_name" {
-  description = "Name of the new App Service Plan"
-}
-
-variable "env" {
-  type        = string
-  description = "The Environment Name of the application"
-}
-
-variable "tier" {
-  type        = string
-  description = "The Tier of the Webapp"
-}  
-
-variable "size" {
-  type        = string
-  description = "The SKU  Size of the Webapp"
-}
-
-variable "location" {
-  type        = string
-  description = "The location where the web app should be created."
-}
-
-variable "resource_group_name" {
+variable "rg_name" {
   type = string
+  default=" "
+}
+variable "image_name" {
+  type = string
+  default=" "
+}
+variable "rg_location" {
+  type = string
+  default=" "
+}
+variable "asp_name" {
+  type = string
+  default=" "
+}
+variable "as_name" {
+  type = string
+  default=" "
+}
+variable "Application_Type" {
+  type = string
+  default="App service"
+}
+variable "Application_Name" {
+  type = string
+  default="Food alert"
+}
+variable "Application_Environment" {
+  type = string
+  default="Dev"
+}
+variable "Brand_name" {
+  type = string
+  default="Food alert"
+}
+variable "Business_Unit" {
+  type = string
+  default="Food alert"
+}
+variable "Product" {
+  type = string
+  default="Food alert"
+}
+variable "Cost_center" {
+  type = string
+  default="Food alert Dev"
+}
+variable "Deploy_type" {
+  type = string
+  default="Terraform"
+}
+variable "Access_type" {
+  type = string
+  default="Private"
+}
+variable "Shared_Resources" {
+  type = string
+  default="Yes"
+}
+variable "Shared_by" {
+  type = string
+  default="Dev"
+}
+variable "Service_Type" {
+  type = string
+  default="API"
+}
+variable "Created_On" {
+  type = string
+  default="30/03/2023 "
+}
+variable "JIRA_ID" {
+  type = string
+  default=" "
+}
+variable "Auto_scale" {
+  type = string
+  default="No"
 }
 
-# variable "ip_restrictions" {
-#   type        = list(string)
-#   default     = []
-#   description = "A list of IP addresses in CIDR format specifying Access Restrictions."
-# }
+variable "Start_Stop" {
+  type = string
+  default="10 AM"
+}
