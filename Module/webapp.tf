694c93069bf90c73831a9b07a5915b7edf6dd17e
udayprakash1997 <123154451+udayprakash1997@users.noreply.github.com>
2023-04-11 20:07:33 +0530

    Update webapp.tf

--- Module/webapp.tf
+++ Module/webapp.tf
@@ -1,3 +1,15 @@
+resource "azurerm_app_service_plan" "aspname" {
+  name                = var.asp_name
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
 resource "azurerm_app_service" "asname" {
   name                = var.as_name
   location            = var.rg_location
